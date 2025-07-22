7a068b150edff4fe843bbf8765b4aa14b584135e
Vildan Mullagaliyev <mullagalievvildan07@gmail.com>
2025-07-22 10:19:39 +0500

    change firebases configuration

--- routers/firebases.py
+++ routers/firebases.py
@@ -1,9 +1,16 @@
+import os
 from fastapi import APIRouter, HTTPException
 from firebase_admin import credentials, messaging, initialize_app
 
+firebase_router = APIRouter(tags=['Firebase'])
 
-firebase_router = APIRouter(tags=['Firebases'])
+FIREBASE_KEY = os.getenv("FIREBASE_KEY")
 
+if FIREBASE_KEY:
+    with open("firebase_temp.json", "w") as f:
+        f.write(FIREBASE_KEY)
 
-cred = credentials.Certificate("serviceAccountKey.json")
-initialize_app(cred)
+    cred = credentials.Certificate("firebase_temp.json")
+    initialize_app(cred)
+else:
+    cred = None  # Можно обработать иначе
