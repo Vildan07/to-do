f03993448f9385ed992110fcdaa7c2d8041d3fa3
Vildan Mullagaliyev <mullagalievvildan07@gmail.com>
2025-07-28 14:55:24 +0500

    Add pymysql for Railway MySQL connection

--- database.py
+++ database.py
@@ -3,11 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine("mysql+pymysql://root:lccGZarSnbELATjAoMWOyIHYpnnbjKLY@mysql.railway.internal:3306/railway")
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def get_db():
